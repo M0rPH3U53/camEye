e444d925c9a7db91ccf2d1b19138ba310c827007
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2026-07-02 09:59:50 +0200

    Update camEye.sh

--- camEye.sh
+++ camEye.sh
@@ -15,10 +15,6 @@ RESET='\033[0m'
 BLANC='\033[1;37m'
 JAUNE='\033[0;33m'
 
-echo -ne "${BLEU}[i]${RESET} ${BLANC}Nom:${RESET} "
-read name
-echo " "
-
 # Cree dossier
 mkdir -p ONVIF
 
@@ -35,7 +31,7 @@ echo " "
 echo -ne "${BLEU}[i]${RESET} ${BLANC}Network:${RESET} "
 read network
 
-# Découverte réseau d'appareil WS-Discover
+# Découverte réseau WS-Discover
 echo -ne "🔍 ${BLANC}Scan WS-Discover${RESET}..."
 urls=$(nmap -sU -sC -p 3702 --open ${network} | grep Address | grep -v MAC | awk '{print $3}')
 echo -e "${JAUNE}100%${RESET}"
@@ -49,6 +45,9 @@ fi
 # Chemin du fichier
 dir=$(pwd)
 
+# Regexe pour recupere ip
+hotes=$(echo ${urls} | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}')
+
 echo " "
 echo -e "${VERT}[+]${RESET} ${BLANC}Camera${RESET}"
 echo " "
@@ -60,7 +59,7 @@ for url in ${urls}; do
     curl -sS -X POST "${url}" \
         -H "Content-Type: application/soap+xml; charset=utf-8" \
         -H 'SOAPAction: "http://www.onvif.org/ver10/device/wsdl/GetDeviceInformation"' \
-        --data-binary @- <<'EOF' > ${dir}/ONVIF/cameye-${name}.xml
+        --data-binary @- <<'EOF' > ${dir}/ONVIF/cameye-${hotes}.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope"
               xmlns:tds="http://www.onvif.org/ver10/device/wsdl">
